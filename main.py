d1c3444caab440a69eae1ffb7774df4a33a53016
IsravelTECH <israveltech@gmail.com>
2026-03-10 13:04:20 +0530

    middlewareadder

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
 from routes import user_routes
 from routes import parking_routes
+from fastapi.middleware.cors import CORSMiddleware
+
 
 
 app = FastAPI()
 
 app.include_router(user_routes.router)
 app.include_router(parking_routes.router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # allow all domains (for testing)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)

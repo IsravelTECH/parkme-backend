b18d6a771070890a92eca1f21e6fcd7299b9c29e
IsravelTECH <israveltech@gmail.com>
2026-03-29 00:44:22 +0530

    newapis

--- routes/stripe_routes.py
+++ routes/stripe_routes.py
@@ -38,8 +38,8 @@ async def create_checkout_session(request: Request):
             "quantity": 1,
         }],
         mode="payment",
-        success_url="https://your-frontend-url/paymentsuccess.html",
-        cancel_url="https://your-frontend-url/payment1.html",
+        success_url="https://127.0.0.1:5500/frontend/paymentsuccess.html",
+        cancel_url="https://127.0.0.1:5500/frontend/payment1.html",
     )
 
     return {"id": session.id}
